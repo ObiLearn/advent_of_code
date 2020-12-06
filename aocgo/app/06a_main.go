37b48ca13f4c45d86c23cece9c0643e0215497d2
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-12-06 18:52:23 +0100

    switch to channels

--- aocgo/app/06a_main.go
+++ aocgo/app/06a_main.go
@@ -13,19 +13,25 @@ func main() {
 	result := 0
 	set := make(map[rune]int)
 
-	extio.ForInputLine(func(line string) {
-		if line == "" {
-			//count reset
-			for _, value := range set {
-				result += value
-			}
-			set = make(map[rune]int)
-		} else {
-			for _, value := range line {
-				set[value] = 1
-			}
-		}
-	})
+    lines, errs := extio.InputLines()
+
+    for line := range lines {
+        if line == "" {
+            //count reset
+            for _, value := range set {
+                result += value
+            }
+            set = make(map[rune]int)
+        } else {
+            for _, value := range line {
+                set[value] = 1
+            }
+        }
+    }
+
+    if err := <-errs; err != nil {
+        log.Fatalf("Error when reading input: %v", err)
+    }
 
 	end := time.Now()
 	log.Printf("total time %s", end.Sub(start))
---
+++ aocgo/pkg/extio/forinputline.go
@@ -0,0 +1,61 @@
+package extio
+
+import (
+    "io"
+)
+
+func ForInputLine(cb func(string)) error {
+	reader, err := PipeReader()
+	if err != nil {
+        return err
+	}
+
+	for {
+		line, err := Readln(reader)
+
+		if err != nil {
+			if err == io.EOF {
+				//break loop on EOF
+				break
+			} else {
+                return err
+			}
+		}
+
+		cb(line)
+	}
+
+    return nil
+}
+
+func InputLines() (<-chan string, <-chan error) {
+    out := make(chan string)
+    errs := make(chan error)
+
+    reader, err := PipeReader()
+	if err != nil {
+        errs <- err
+        close(out)
+        close(errs)
+		return out, errs
+	}
+
+    go func() {
+        defer close(out)
+        defer close(errs)
+
+        for {
+            line, err := Readln(reader)
+
+            if err != nil {
+                if err != io.EOF {
+                    errs <- err
+                }
+                break
+            }
+            out <- line;
+        }
+    }()
+
+    return out, errs
+}

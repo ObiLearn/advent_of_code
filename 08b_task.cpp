6f488cfe7b105a6882e8f5da8ee39f572b770555
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-12-08 23:14:57 +0100

    Solve 8b.

--- 08b_task.cpp
+++ 08b_task.cpp
@@ -1,3 +1,4 @@
+#include <bits/c++config.h>
 #include <iostream>
 #include <string>
 #include <string_view>
@@ -42,47 +43,22 @@ cmd v2e(std::string_view const& v) {
     return cmd::invalid;
 }
 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+using memory_t = std::vector<std::pair<cmd,std::int32_t>>;
 
-    ext::util::scoped_timer timer{};
-
-
-    std::vector<std::pair<cmd,std::int32_t>> memory;
-    memory.emplace_back(std::pair{cmd::invalid,0});
-    std::string line;
-    while(std::getline(std::cin, line)) {
-        //std::cout << "<< " << line << std::endl;
-
-        auto cmd_view = std::string_view(line.data(), 3);
-        auto num_view = std::string_view(line.data()+5, line.size()-5);
-
-        int num = 0;
-        std::from_chars(num_view.data(), num_view.data()+num_view.size(),num);
-        if (line[4] == '-') {
-            num *= -1;
-        }
-
-        memory.emplace_back(std::pair{v2e(cmd_view), num});
-
-        using ext::util::pretty::operator<<;
-        //std::cout << (int) memory.back().first << " "  << memory.back().second << std::endl;
-    };
-    timer.add_step("input read");
-
+std::pair<std::size_t,std::size_t> simulate(memory_t const& memory){
     std::unordered_set<std::size_t> visited;
     std::size_t current=1;
     std::int64_t acc=0;
 
     while (true) {
         if(!visited.empty() && visited.find(current) != visited.end()) {
-            std::cout << current << " found in visited" << std::endl;
-            //using ext::util::pretty::operator<<;
-            //std::cout << visited << std::endl;
             break;
         }
         visited.insert(current);
+
+        if(current >= memory.size()) {
+            return {acc, current};
+        }
 
         auto[cmd,num] = memory.at(current);
 
@@ -101,16 +77,65 @@ int main() {
                 break;
             }
             case cmd::invalid: {
-                std::cout << current << " found in visited" << std::endl;
                 using ext::util::pretty::operator<<;
+                std::cout << current << " found in visited" << std::endl;
                 std::cout << visited << std::endl;
                 throw std::runtime_error("invalid input");
             }
         }
 
     }
-    timer.add_step("simulation done");
 
-    std::size_t result = acc;
+    return {acc, current};
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    ext::util::scoped_timer timer{};
+
+
+    std::vector<std::pair<cmd,std::int32_t>> memory;
+    memory.emplace_back(std::pair{cmd::invalid,0});
+    std::string line;
+    while(std::getline(std::cin, line)) {
+        auto cmd_view = std::string_view(line.data(), 3);
+        auto num_view = std::string_view(line.data()+5, line.size()-5);
+
+        int num = 0;
+        std::from_chars(num_view.data(), num_view.data()+num_view.size(),num);
+        if (line[4] == '-') {
+            num *= -1;
+        }
+
+        memory.emplace_back(std::pair{v2e(cmd_view), num});
+    };
+
+    timer.add_step("input read");
+
+    std::size_t result = 0;
+    std::size_t current = 0;
+
+    for(std::size_t i = 1; i < memory.size(); ++i) {
+
+        auto cell = memory[i];
+
+        switch (cell.first) {
+            case cmd::jmp: memory[i].first = cmd::nop; break;
+            case cmd::nop: memory[i].first = cmd::jmp; break;
+            default: continue;
+        }
+
+        std::tie(result, current) = simulate(memory);
+
+        if (current == memory.size()) break;
+
+        memory[i] = cell;
+
+    }
+
+
+    timer.add_step("simulation done");
     std::cout << "\nresult:" << result << "\n";
 }

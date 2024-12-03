3cb246f27b82120cf0cb613797372fd2c7271db4
Jan Christoph Uhde <Jan@UhdeJC.com>
2024-12-03 21:15:48 +0100

    add: solution for 2024-12-02 - part 2

--- 2024/02/app.cpp
+++ 2024/02/app.cpp
@@ -13,13 +13,15 @@ auto parse_input() {
     std::vector<std::vector<int>> vec;
 
     util::input_by_line([&](std::string const& line) -> void {
-            std::vector<std::string> strs;
-            boost::algorithm::split(strs, line, [](const char x){ return x == ' ';});
-            std::vector<int> ints(strs.size());
-            std::transform(strs.begin(), strs.end(), ints.begin(), [](std::string const& x){
-                return util::int_from_iterators(x.begin(), x.end());
-            });
-            vec.push_back(std::move(ints));
+        std::vector<std::string> strs;
+        boost::algorithm::split(strs, line, [](const char x) {
+            return x == ' ';
+        });
+        std::vector<int> ints(strs.size());
+        std::transform(strs.begin(), strs.end(), ints.begin(), [](std::string const& x) {
+            return util::int_from_iterators(x.begin(), x.end());
+        });
+        vec.push_back(std::move(ints));
     });
 
     return vec;
@@ -31,6 +33,40 @@ int main() {
     // part 1
     {
         auto check_vec = [](std::vector<int> const& vec) {
+            if (vec.size() == 0)
+                return false;
+            if (vec.size() == 1)
+                return true;
+
+            int last = vec[0];
+            bool asc = vec[0] < vec[1];
+            for (auto it = vec.begin() + 1; it != vec.end(); ++it) {
+                auto curr = *it;
+                if (asc && (curr < last))
+                    return false;
+                if (!asc && (curr > last))
+                    return false;
+
+                int abs = std::abs(curr - last);
+                if (abs < 1 || abs > 3)
+                    return false;
+
+                last = curr;
+            }
+            return true;
+        };
+
+        int res = 0;
+        for (auto const& vec : vecs) {
+            if (check_vec(vec))
+                res += 1;
+        }
+        std::cout << "result part 1: " << res << std::endl;
+    }
+    // part 2 - ranges unoptimized
+    /*
+    {
+        auto check_range = [](std::ranges::random_access_range auto const& vec) {
             if(vec.size() == 0)
                 return false;
             if(vec.size() == 1)
@@ -54,11 +90,63 @@ int main() {
         };
 
         int res = 0;
-        for (auto const& vec : vecs)
-        {
-            if (check_vec(vec))
+        for (auto const& vec : vecs) {
+            if (check_range(vec))
                 res += 1;
+            else {
+                for(std::size_t i = 0; i < vec.size(); ++i) {
+                    const auto filter_func = [&](auto &&pair){ return std::get<0>(pair) != i; };
+                    auto view = vec | std::views::enumerate | std::views::filter(filter_func) | std::views::values;  // <-- only bidi range
+                    if (check_range(view)) {
+                        res += 1; break;
+                    }
+                }
+            }
         }
-        std::cout << "result part 1: " << res << std::endl;
+        std::cout << "result part 2: " << res << std::endl;
+    }
+    */
+    // part 2 - unoptimized copy:(
+    {
+        auto check_range = [](std::ranges::random_access_range auto const& vec) {
+            if (vec.size() == 0)
+                return false;
+            if (vec.size() == 1)
+                return true;
+
+            int last = vec[0];
+            bool asc = vec[0] < vec[1];
+            for (auto it = vec.begin() + 1; it != vec.end(); ++it) {
+                auto curr = *it;
+                if (asc && (curr < last))
+                    return false;
+                if (!asc && (curr > last))
+                    return false;
+
+                int abs = std::abs(curr - last);
+                if (abs < 1 || abs > 3)
+                    return false;
+
+                last = curr;
+            }
+            return true;
+        };
+
+        int res = 0;
+        for (auto const& vec : vecs) {
+            if (check_range(vec))
+                res += 1;
+            else {
+                for (std::size_t i = 0; i < vec.size(); ++i) {
+                    auto copy = vec;
+                    copy.erase(copy.begin() + i);
+                    if (check_range(copy)) {
+                        res += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        std::cout << "result part 2: " << res << std::endl;
     }
 }
